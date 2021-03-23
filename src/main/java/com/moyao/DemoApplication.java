6760aeb3a779a4f82ae3abd0ed73c393217370b0
李忠央 <li.zhongyang@darcytech.com>
2021-03-23 23:01:32 +0800

    ddddd

--- src/main/java/com/moyao/DemoApplication.java
+++ src/main/java/com/moyao/DemoApplication.java
@@ -1,8 +1,10 @@
 package com.moyao;
 
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@DubboComponentScan
 @SpringBootApplication
 public class DemoApplication {
 
